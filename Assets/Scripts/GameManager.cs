bb6a2054809ec7dfbe601709b9d3cb9add1dec21
Patrick Boyer <boyer.patrick@gmail.com>
2017-01-20 20:29:34 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject selectionStrip;
+    public GameObject theTankIcon;
+
+    private float spawnTimer = 0f;
+    public float spawnCooldown = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > spawnCooldown)
+        {
+            spawnTimer = 0f;
+            GameObject TankGO = Instantiate(theTankIcon, Vector3.zero, Quaternion.identity) as GameObject;
+            TankIcon newIcon = TankGO.GetComponent<TankIcon>();
+            newIcon.theSprite = Resources.Load<Sprite>("Sprites/Tank3Sprite");
+            Debug.Log("Hello");
+            //create a new tile in the strip
+        }
+	}
+}
---
+++ Assets/Scripts/TankIcon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TankIcon : MonoBehaviour {
+
+    public Sprite theSprite;
+    private Image theImage;
+    public float velocity = 1f;
+    
+    // Use this for initialization
+	void Start () {
+        theImage = GetComponent<Image>();
+        theImage.sprite = theSprite;    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(0f, velocity * Time.deltaTime, 0f);
+	}
+}
