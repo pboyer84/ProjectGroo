ec1f7bd2f1f32e5e04bbb7b0cf75eba9b582ecdc
Incubating <sparkinatordott@gmail.com>
2017-01-20 20:53:39 -0500

    Initial Commit(Seth)

---
+++ Assets/Scripts/GenerateTank.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateTank : MonoBehaviour {
+    public GameObject tankPrefab;
+    int spawnNumber = 0;
+    public Vector3[] spawnPoints;
+    Vector3 spawnPosition;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space")) {
+            //three points, three unity units apart, cycled through
+            spawnPosition = spawnPoints[spawnNumber];
+            GameObject.Instantiate(tankPrefab, spawnPosition, Quaternion.identity);
+            if(spawnNumber == 2){
+                spawnNumber = 0;
+            }else{
+                spawnNumber++;
+            }
+        }
+	}
+}
