a496af9713c874dd4fe6250f6db43bc10d89f8ae
Patrick Boyer <boyer.patrick@gmail.com>
2017-01-21 23:25:54 -0500

    health bars

---
+++ Assets/Scripts/BillboardingHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BillboardingHealthBar : MonoBehaviour {
+
+    private Image healthBarImage;
+    private float fullHealthBarLength;
+    private Health myHealth;
+
+	void Start ()
+    {
+        myHealth = GetComponentInParent<Health>();
+        fullHealthBarLength = myHealth.value;
+        healthBarImage = GetComponentInChildren<Image>();
+        healthBarImage.gameObject.transform.rotation = Camera.main.transform.rotation;
+    }
+	
+	void Update ()
+    {
+        healthBarImage.fillAmount = myHealth.value / fullHealthBarLength;
+    }
+
+    void LateUpdate()
+    {
+        healthBarImage.gameObject.transform.rotation = Camera.main.transform.rotation;
+    }
+}
